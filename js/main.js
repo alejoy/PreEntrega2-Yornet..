5d8f4f302753537a3d34ff9507fde0ca9be135c7
alejoy <alejoyornet@gmail.com>
2023-04-18 11:53:45 -0300

    Fix

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ function calcular() {
   //const app = document.getElementById("simulador");
   const bancos = document.querySelector('input[name="banco"]:checked').value;
   let tna = 78/100;
-  console.log(tna)
+  //console.log(tna)
   if (bancos==1){
   tna = 78 / 100;}
   if (bancos==2){
@@ -15,28 +15,36 @@ function calcular() {
   const deposito = document.getElementById("capital").value;
   const duracion = document.getElementById("duracion").value;
    
-    if(deposito!='' && tna!='' && duracion>='30' && duracion<='365' ){
+    if(deposito!='' && duracion!=''){
     const cap = deposito * (1 + tna * (duracion / 365));
     const interes =  cap - deposito;
-          if (cap >= 1000) {
-          $('#calc_result').text('$' + cap.toFixed(2));
-          $('#calc_interes').text('$' + interes.toFixed(2));
-          $('#tna').text(100*tna.toFixed(2)+"%");
-          $('#dias').text( duracion + ' dias');
-          $('#monto').text('$' + deposito);
-          $('#clac_error').text('');
-          $('#duracion').element.classList.remove('danger');
-          $('#capital').element.classList.remove('danger');
-      } 
-      else {
-        $('#capital').element.classList.add('danger');
-        $('#clac_error').text('Ingrese valor mayor a $1000.');
-        
-      }
-      
-    }else{
-        $('#clac_error').text('Ingrese un valor de días mayor a 30 o menor a 365');
-        $('#duracion').element.classList.add('danger');
-    } 
-
+      if (deposito >= 1000) {
+        let result = '$' + cap.toFixed(2);
+        document.getElementById("calc_result").innerHTML = result;
+        let calc_interes = '$' + interes.toFixed(2);
+        document.getElementById("calc_interes").innerHTML = calc_interes;
+        let dias = duracion + " dias";
+        document.getElementById("dias").innerHTML = dias;  
+        let monto = '$' + deposito;
+        document.getElementById("monto").innerHTML = monto;
+        let calc_error = "";
+        document.getElementById("calc_error").innerHTML = calc_error;
+        let tasa = 100 * tna + '%';
+        document.getElementById("tna").innerHTML = tasa;
+          }           
+          else {
+            alert("menos mil") 
+            calc_error = 'Ingrese valor mayor a $1000';
+            document.getElementById("calc_error").innerHTML = calc_error;
+          }
+      if(duracion <'30' || duracion>'365'){
+        calc_error = 'Ingrese un valor de días mayor a 30 o menor a 365';
+        document.getElementById("calc_error").innerHTML = calc_error;
+        } else {
+          calc_error = '';
+        }
+    }
+    else {
+      calc_error = 'Debe ingresar un valor en los campos solicitados';
+    }
 }
